a0ff932fe8f72ae6731f190c7a237ca268ee3d79
Driver <cloudorzi@gmail.com>
2020-02-19 19:06:08 +0800

    clean commits

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# My-action
+# My-Action
+Personal-Action
+本项目仅供本人使用
+
+构建状态：
+x86_64 LEDE ![Build and Upload Lean's x86_64 LEDE](https://github.com/1orz/My-action/workflows/Build%20and%20Upload%20Lean's%20x86_64%20LEDE/badge.svg)
+
+RaspberryPi LEDE：![Build and Upload Lean's RaspberryPi LEDE](https://github.com/1orz/My-action/workflows/Build%20and%20Upload%20Lean's%20RaspberryPi%20LEDE/badge.svg)
+
---
+++ lean-lede/rpi/diy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# 本脚本工作目录必须是git clone的主目录
+# Raspberry
+
+# Add Luci-theme
+packages_path=package/openwrt-packages
+mkdir -p $packages_path
+
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial $packages_path/luci-theme-atmaterial
+git clone https://github.com/Ameykyl/luci-app-koolproxyR $packages_path/luci-app-koolproxyR
+git clone https://github.com/rosywrt/luci-theme-rosy $packages_path/luci-theme-rosy
+
+# Modify default IP
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
+
+# Add some default settings
+#\cp -rf ../lede/zzz-default-settings package/lean/default-settings/files/zzz-default-settings
+curl -fsSL https://raw.githubusercontent.com/1orz/My-action/master/lean-lede/rpi/zzz-default-settings > package/lean/default-settings/files/zzz-default-settings
