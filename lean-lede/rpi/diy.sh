468bf362ac0d05d8b4855ea8e0d5029594b80ff6
Driver <cloudorzi@gmail.com>
2020-03-19 15:49:03 +0800

    Update diy.sh

--- lean-lede/rpi/diy.sh
+++ lean-lede/rpi/diy.sh
@@ -2,13 +2,13 @@
 # 本脚本工作目录必须是git clone的主目录
 # Raspberry
 
-# Add Luci-theme
 packages_path=package/openwrt-packages
 mkdir -p $packages_path
 
 git clone https://github.com/openwrt-develop/luci-theme-atmaterial $packages_path/luci-theme-atmaterial
-git clone https://github.com/Ameykyl/luci-app-koolproxyR $packages_path/luci-app-koolproxyR
-git clone https://github.com/rosywrt/luci-theme-rosy $packages_path/luci-theme-rosy
+git clone https://github.com/frainzy1477/luci-app-clash $packages_path/luci-app-clash
+git clone https://github.com/vernesong/OpenClash $packages_path/OpenClash
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  $packages_path/luci-theme-argon
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
