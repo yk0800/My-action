27ac8c31ba5b47854fe6f34fde2ec8f9a5c37dfd
Driver <cloudorzi@gmail.com>
2020-03-29 18:52:37 +0800

    Update diy.sh

--- lean-lede/x86_64/diy.sh
+++ lean-lede/x86_64/diy.sh
@@ -18,6 +18,9 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  $packages_
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
 
+# Modify default Theme
+sed -i 's/bootstrap/argon/g' feeds/luci/modules/luci-base/root/etc/config/luci
+
 # Add some default settings
 #\cp -rf ../lede/zzz-default-settings package/lean/default-settings/files/zzz-default-settings
 curl -fsSL https://raw.githubusercontent.com/1orz/My-action/master/lean-lede/x86_64/zzz-default-settings > package/lean/default-settings/files/zzz-default-settings
